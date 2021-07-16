a63367db4cbb44da57dbb3a40f4164fa1d2c2ee3
mattbizzo <bizzo.mateus@gmail.com>
2021-07-16 10:29:06 -0300

    add testes

--- tests/test_avaliador.py
+++ tests/test_avaliador.py
@@ -1,11 +1,27 @@
 from unittest import TestCase
+from src.leilao.dominio import Avaliador, Usuario, Lance, Leilao
 
 class TestAvaliador(TestCase):
 
     def test_avalia(self):
-        # valor esperado/valor_informado
-        valor_esperado = 1
-        valor_informado = 1
-        self.assertEqual(valor_esperado,valor_informado)
+        mat = Usuario('Mateus')
+        re = Usuario('Regina')
+
+        lance_mat = Lance(mat, 100.00)
+        lance_re = Lance(re, 150.00)
+
+        leilao = Leilao('Celular')
+
+        leilao.lances.append(lance_mat)
+        leilao.lances.append(lance_re)
+
+        avaliador = Avaliador()
+        avaliador.avalia(leilao)
+
+        min_esperado = 100.00
+        max_esperado = 150.00
+
+        self.assertEqual(max_esperado, avaliador.maior_lance)
+        self.assertEqual(min_esperado, avaliador.menor_lance)
 
         
