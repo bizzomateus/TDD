7d78de10cc96ce66df0a4aab89ebf535573da3e1
mattbizzo <bizzo.mateus@gmail.com>
2021-08-01 09:42:06 -0300

    refatora nome de classe teste

--- src/test_leilao.py
+++ src/test_leilao.py
@@ -2,7 +2,7 @@ from unittest import TestCase
 from leilao.dominio import Usuario, Lance, Leilao
 
 
-class TestAvaliador(TestCase):
+class TestLeilao(TestCase):
 
     def setUp(self):
         self.mat = Usuario('Mateus')
